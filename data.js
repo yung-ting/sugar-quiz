f57382f3cd31f225a4e6d8c95735e77070d34b60
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-05 16:55:50 +0200

    adding basic files

---
+++ data.js
@@ -0,0 +1,16 @@
+let products = [
+    {
+      "title": "The Shawshank Redemption",
+      "year": 1994,
+      "director": "Frank Darabont",
+      "duration": "2h 22min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 9.3
+    },
+    {
+      
+    }
+]
