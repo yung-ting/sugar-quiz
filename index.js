5754720afe181effc964cab78f73f92af63e88c6
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-06 18:34:06 +0200

    adding second button

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 let productQtyElem = document.getElementById("product-qty");
 let productNameElem = document.getElementById("product-name");
 let productImgElem = document.getElementById("product-img");
+let inputField = document.getElementById("guessOfUser");
+let rightOrWrong = document.getElementById("right-or-wrong");
+let productSugarElem = document.getElementById("outcome-sugar-cubes");
+let productIndx = 0;
 
 function updateProducts() {
-    let productIndx = Math.floor(Math.random() * 10);
+    productIndx = Math.floor(Math.random() * 10);
     productQtyElem.innerText = products[productIndx].qty;
     productNameElem.innerText = products[productIndx].name;
     productImgElem.src = products[productIndx].image;
+    inputField.value = 0;
+    document.getElementById('outcome-text').innerHTML = "";
 }
-updateProducts();
-
 
 function compareNumbers() {
     //when click on check-fact btn, it compares input number with products.cubeCount
     let inputCount = document.getElementById('guessOfUser').value;
-    document.getElementById('demo').innerHTML = inputCount;
+    //document.getElementById('demo').innerHTML = inputCount;
+    
+    if (inputCount === products[productIndx].cubeCount) {
+    
+        productSugarElem.innerText = products[productIndx].cubeCount;
+        document.getElementById('outcome-text').innerHTML = `Exactly! It contains ${productSugarElem.innerText} cubes!`
+    }
+    else {
+        productSugarElem.innerText = products[productIndx].cubeCount;
+        document.getElementById('outcome-text').innerHTML = `Nope! It contains ${productSugarElem.innerText} cubes!`
+    }
 }
 
+//EventListeners
 document.querySelector('.btn-check-fact').addEventListener('click', () => {
     compareNumbers();
+  });
+
+document.querySelector('.btn-next-product').addEventListener('click', () => {
+    updateProducts();
   });
