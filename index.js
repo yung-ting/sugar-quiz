3014380582ef5190931a6b4177275c6dcb6af78d
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-06 16:54:13 +0200

    display the data and adding one button

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+let productQtyElem = document.getElementById("product-qty");
+let productNameElem = document.getElementById("product-name");
+let productImgElem = document.getElementById("product-img");
+
+function updateProducts() {
+    let productIndx = Math.floor(Math.random() * 10);
+    productQtyElem.innerText = products[productIndx].qty;
+    productNameElem.innerText = products[productIndx].name;
+    productImgElem.src = products[productIndx].image;
+}
+updateProducts();
+
+
 function compareNumbers() {
     //when click on check-fact btn, it compares input number with products.cubeCount
     let inputCount = document.getElementById('guessOfUser').value;
@@ -7,8 +20,3 @@ function compareNumbers() {
 document.querySelector('.btn-check-fact').addEventListener('click', () => {
     compareNumbers();
   });
-
-//   function myFunction() {
-//     var x = document.getElementById("myNumber").value;
-//     document.getElementById("demo").innerHTML = x;
-//   }
