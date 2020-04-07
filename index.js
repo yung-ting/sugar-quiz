bb04bdeb1cea68b83777a3af0a704d6674e882da
yung-ting-chang <tina.yt.chang@protonmail.com>
2020-04-07 21:14:51 +0200

    adding start and result buttons

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let rightOrWrong = document.getElementById("right-or-wrong");
 let productSugarElem = document.getElementById("outcome-sugar-cubes");
 let productIndx = 0;
 let questionCounts = document.getElementById('points-base');
+let pointCounts = document.getElementById('points');
+let resultTexts = document.getElementById('result-text');
+let startPage = document.getElementById('start-page');
 //hide and show
 let hiddenBtn = document.querySelector('.btn-next-product');
 let showBtn = document.querySelector('.btn-see-results');
@@ -26,6 +29,19 @@ function updateProducts() {
     accumQuestions ++
 }
 
+function reloadMainPage() {
+    productIndx = Math.floor(Math.random() * products.length);
+    productQtyElem.innerText = products[productIndx].qty;
+    productNameElem.innerText = products[productIndx].name;
+    productImgElem.src = products[productIndx].image;
+    inputField.value = 0;
+    document.getElementById('outcome-text').innerHTML = "";
+    questionCounts.innerHTML = 1;
+    accumQuestions = 1;
+    pointCounts.innerHTML = 0;
+    resultTexts.innerHTML = '';
+}
+
 function compareNumbers() {
     //when click on check-fact btn, it compares input number with products.cubeCount
     let inputCount = document.getElementById('guessOfUser').value;
@@ -34,26 +50,36 @@ function compareNumbers() {
     if (inputCount == Math.round(products[productIndx].cubeCount)) {
         productSugarElem.innerText = Math.round(products[productIndx].cubeCount);
         document.getElementById('outcome-text').innerHTML = `Exactly! It contains about ${productSugarElem.innerText} cubes!`
-        document.getElementById('points').innerHTML ++
+        pointCounts.innerHTML ++
     }
     else {
         productSugarElem.innerText = Math.round(products[productIndx].cubeCount);
         document.getElementById('outcome-text').innerHTML = `Nope! It contains about ${productSugarElem.innerText} cubes!`
     }
 }
-//======
-function showNextProd() {
+
+function showNextProdBtn() {
     if (accumQuestions == 5) {
         hiddenBtn.style.visibility = 'hidden';
+    } else {
+        hiddenBtn.style.visibility = 'visible';
     }
 }
 
-function showResultBtn() {
+function hideOrShowResultBtn() {
     if (accumQuestions >= 5) {
         showBtn.style.display = "block";
     } else {
         showBtn.style.display = "none";
     }
+}
+
+function hideStartPageElem() {
+    startPage.style.display = "none";
+}
+
+function showStartPageElem() {
+    startPage.style.display = "block";
 }
 
 function hidePlayAgainBtn() {
@@ -63,7 +89,7 @@ function hidePlayAgainBtn() {
 function showPlayAgainBtn() {
     document.querySelector('.btn-play-again').style.display = "block";
 }
-///======== above are not working
+
 function hideMainPageElem() {
     mainPage.style.display = "none";
 }
@@ -84,7 +110,7 @@ function updateCubesBySec() {
            cubeImgElem.src =  'images/sugar cube.svg'; 
            cubeImgElem.setAttribute('class','sugar-cube-img');
            cubeImgContainer.appendChild(cubeImgElem);
-       }, 220 * i); 
+       }, 100 * i); 
      } 
 }
 
@@ -92,31 +118,60 @@ function resetSugarCubes() {
     cubeImgContainer.innerHTML = "";
 }
 
+function showResultTexts() {
+    if (pointCounts.innerHTML >= 3) {
+        resultTexts.innerHTML = "You are amzing!";
+    } else {
+        resultTexts.innerHTML = "You don't know what you are eating!";
+    }
+}
 
+function disableBtns() {
+    document.querySelector('.btn-check-fact').disabled = true;
+}
 
+function enableBtns() {
+    document.querySelector('.btn-check-fact').disabled = false;
+}
+
+hideMainPageElem();
 updateProducts();
-showResultBtn();
+hideOrShowResultBtn();
 hidePlayAgainBtn();
 
 
 //EventListeners
+document.querySelector('.btn-start').addEventListener('click', () => {
+    showMainPageElem();
+    hideStartPageElem();
+  });
+
 document.querySelector('.btn-check-fact').addEventListener('click', () => {
     compareNumbers();
     updateCubesBySec();
+    disableBtns();
   });
 
 document.querySelector('.btn-next-product').addEventListener('click', () => {
     updateProducts();
     resetSugarCubes();
-    showNextProd();
-    showResultBtn();
+    showNextProdBtn();
+    hideOrShowResultBtn();
+    enableBtns();
   });
 
 document.querySelector('.btn-see-results').addEventListener('click', () => {
     hideMainPageElem();
     showPlayAgainBtn();
+    showResultTexts();
 });
 
 document.querySelector('.btn-play-again').addEventListener('click', () => {
+    reloadMainPage();
     showMainPageElem();
+    resetSugarCubes();
+    
+    hidePlayAgainBtn();
+    showNextProdBtn();
+    hideOrShowResultBtn();
 });
